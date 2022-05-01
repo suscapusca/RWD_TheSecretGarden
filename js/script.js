c12a8ac88db011a9b17e5a3713449d4cffd35a3e
WalidMoua <walidmou97@gmail.com>
2022-05-01 12:37:07 +0200

    Adding template files

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+while(true){
+    
+    let randomX =Math.floor(Math.random()*screen.width)
+    let randomY = Math.floor(Math.random()*screen.height)
+     $("#butterfly").css({"left" : randomX, "top" : randomY});
+    
+    
+}
